e53179e81942c2ef2d1d19a5eaf6663f3e2e5813
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-01-04 23:36:44 +0530

    First commit

---
+++ src/library/mgmt/system/Project.java
@@ -0,0 +1,26 @@
+package library.mgmt.system;
+
+import java.util.Scanner;
+
+public class Project
+{
+	public static void main(String[] args)
+	{
+		System.out.println("**************Library Management System*****************");
+		System.out.println("Press 1 to add a book");
+		System.out.println("Press 2 to issue a book");
+		System.out.println("Press 3 to return a book");
+		System.out.println("Press 4 to print complete issue details");
+		System.out.println("Press 5 to exit");
+	}
+}
+
+class Library
+{
+	void add()
+	{
+		System.out.println("Enter book name, prize and book number.");
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+	}
+}
