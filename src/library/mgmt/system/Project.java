1b031ce5bbb7007e334a824c2fb139d153b8a6d7
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-01-05 23:37:30 +0530

    added new features

--- src/library/mgmt/system/Project.java
+++ src/library/mgmt/system/Project.java
@@ -7,6 +7,7 @@ public class Project
 	public static void main(String[] args)
 	{
 		System.out.println("**************Library Management System*****************");
+		System.out.println();
 		System.out.println("Press 1 to add a book");
 		System.out.println("Press 2 to issue a book");
 		System.out.println("Press 3 to return a book");
@@ -21,6 +22,14 @@ class Library
 	{
 		System.out.println("Enter book name, prize and book number.");
 		Scanner s = new Scanner(System.in);
-		String str = s.nextLine();
+		String bookName = s.nextLine();
+		float price = s.nextInt();
+		int bookNo = s.nextInt();
+		System.out.println("Your details are "+bookName+" "+price+" "+bookNo);
+	}
+	
+	void issue()
+	{
+		
 	}
 }
